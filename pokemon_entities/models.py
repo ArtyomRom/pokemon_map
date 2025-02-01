80eead9c1717dea26b4f191564be31cc13c415fc
Artyom <zik21b@gmail.com>
2025-02-01 19:29:56 +0300

    add Pokemon.image model field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,12 +3,12 @@ from django.db import models, migrations  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-
+    image = models.ImageField(upload_to='pokemon_map/pokemon_entities/', blank=True, null=True)
 
     def __str__(self):
         return self.title
 
-    
+
 class Migration(migrations.Migration):
     dependencies = [
         ('pokemon_entities', '0001_initial'),
