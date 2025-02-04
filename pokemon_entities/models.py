a29834837f3d4848036f2ff20b675965638d058f
Artyom <zik21b@gmail.com>
2025-02-04 20:02:40 +0300

    add specs to PokemonEntity model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,5 @@
 from django.db import models, migrations  # noqa F401
-
+from django.utils import timezone
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
@@ -12,8 +12,13 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True)
     lat = models.FloatField()
     lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
+    appeared_at = models.DateTimeField(default=timezone.now)
+    disappeared_at = models.DateTimeField(default=timezone.now)
+    level = models.IntegerField(default=0)
+    health = models.IntegerField(default=0)
+    strength = models.IntegerField(default=0)
+    defence = models.IntegerField(default=0)
+    stamina = models.IntegerField(default=0)
 
     def __str__(self):
         return self.lat, self.lon, self.pokemon
