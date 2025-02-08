88c95faad8b8644aaba0f20ee73447da45024dc9
Artyom <zik21b@gmail.com>
2025-02-08 23:44:50 +0300

    Добавил русскоязычные названия атрибутов на страничку администратора

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,38 +3,40 @@ from django.utils import timezone
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='pokemon_map/pokemon_entities/', blank=True, null=True,
+    '''Покемон'''
+    title = models.CharField('Имя', max_length=200)
+    image = models.ImageField('Картинка', upload_to='pokemon_map/pokemon_entities/', blank=True, null=True,
                               default=None)
-    description = models.TextField(blank=True, null=True, default='Информации о покемоне нет')
-    title_en = models.CharField(max_length=200, blank=True, null=True)
-    title_jp = models.CharField(max_length=200, blank=True, null=True)
-    previous_evolution = models.ForeignKey('self',
+    description = models.TextField('Описание',blank=True, null=True, default='Информации о покемоне нет')
+    title_en = models.CharField('Имя по английски',max_length=200, blank=True, null=True)
+    title_jp = models.CharField('Имя по японски',max_length=200, blank=True, null=True)
+    previous_evolution = models.ForeignKey('self', verbose_name='Предок',
                                            on_delete=models.SET_NULL,
                                            null=True,
                                            related_name='next_evolutions',
-                                           blank=True)
-    next_evolution = models.ForeignKey('self',
+                                           blank=True,)
+    next_evolution = models.ForeignKey('self', verbose_name='Потомок',
                                        on_delete=models.SET_NULL,
                                        null=True,
                                        related_name='previous_evolution_v2',
-                                       blank=True)
+                                       blank=True,)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField(default=timezone.now)
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    '''Сущность покемона'''
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.SET_NULL, null=True)
+    lat = models.FloatField('Долгота', )
+    lon = models.FloatField('Широта', )
+    appeared_at = models.DateTimeField('Появился', default=timezone.now)
+    disappeared_at = models.DateTimeField('Исчез', default=timezone.now)
+    level = models.IntegerField('Уровень', default=0)
+    health = models.IntegerField('Здоровье', default=0)
+    strength = models.IntegerField('Сила', default=0)
+    defence = models.IntegerField('Защита', default=0)
+    stamina = models.IntegerField('Выносливость', default=0)
 
     def __str__(self):
         return f'{self.pokemon}, {self.lat}, {self.lon}'
