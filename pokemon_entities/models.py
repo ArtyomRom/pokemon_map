fb4616671ed71a0c1865644ebcd2b8327542fd03
Artyom <zik21b@gmail.com>
2025-02-09 00:05:01 +0300

    Добавил параметры для атрибутов класса

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,22 +4,22 @@ from django.utils import timezone
 
 class Pokemon(models.Model):
     '''Покемон'''
-    title = models.CharField('Имя', max_length=200)
+    title = models.CharField('Имя', max_length=200, blank=True)
     image = models.ImageField('Картинка', upload_to='pokemon_map/pokemon_entities/', blank=True, null=True,
                               default=None)
-    description = models.TextField('Описание',blank=True, null=True, default='Информации о покемоне нет')
-    title_en = models.CharField('Имя по английски',max_length=200, blank=True, null=True)
-    title_jp = models.CharField('Имя по японски',max_length=200, blank=True, null=True)
+    description = models.TextField('Описание', blank=True, default='Информации о покемоне нет')
+    title_en = models.CharField('Имя по английски', max_length=200, null=True)
+    title_jp = models.CharField('Имя по японски', max_length=200, null=True)
     previous_evolution = models.ForeignKey('self', verbose_name='Предок',
                                            on_delete=models.SET_NULL,
                                            null=True,
                                            related_name='next_evolutions',
-                                           blank=True,)
+                                           blank=True, )
     next_evolution = models.ForeignKey('self', verbose_name='Потомок',
                                        on_delete=models.SET_NULL,
                                        null=True,
                                        related_name='previous_evolution_v2',
-                                       blank=True,)
+                                       blank=True, )
 
     def __str__(self):
         return self.title
@@ -28,10 +28,10 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     '''Сущность покемона'''
     pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.SET_NULL, null=True)
-    lat = models.FloatField('Долгота', )
-    lon = models.FloatField('Широта', )
-    appeared_at = models.DateTimeField('Появился', default=timezone.now)
-    disappeared_at = models.DateTimeField('Исчез', default=timezone.now)
+    lat = models.FloatField('Долгота', blank=True)
+    lon = models.FloatField('Широта', blank=True)
+    appeared_at = models.DateTimeField('Появился', default=timezone.now, blank=True)
+    disappeared_at = models.DateTimeField('Исчез', default=timezone.now, blank=True)
     level = models.IntegerField('Уровень', default=0)
     health = models.IntegerField('Здоровье', default=0)
     strength = models.IntegerField('Сила', default=0)
