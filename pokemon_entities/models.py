9c42c0427e350ea71667211602e283b659440356
Artyom <zik21b@gmail.com>
2025-03-07 21:02:57 +0300

    Изменения
    - related_name у поля pokemon
    - Убрал null в строковых полях
    - Поменял настройки полей

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,9 +6,9 @@ class Pokemon(models.Model):
     title = models.CharField(verbose_name='Имя покемона', max_length=200, blank=False, null=False)
     image = models.ImageField(verbose_name='Изображения покемона', upload_to='pokemon_map/pokemon_entities/', blank=True, null=True,
                               default=None)
-    description = models.TextField(verbose_name='Описание покемона', blank=True, default='Информации о покемоне нет')
-    title_en = models.CharField(verbose_name='Имя по английски', max_length=200, blank=True, null=True)
-    title_jp = models.CharField(verbose_name='Имя по японски', max_length=200, blank=True, null=True)
+    description = models.TextField(verbose_name='Описание покемона', blank=True, null=False, default='Информации о покемоне нет')
+    title_en = models.CharField(verbose_name='Имя по английски', max_length=200, blank=True, null=False)
+    title_jp = models.CharField(verbose_name='Имя по японски', max_length=200, blank=True, null=False)
     previous_evolution = models.ForeignKey('self', verbose_name='Предок',
                                            on_delete=models.SET_NULL,
                                            null=True,
@@ -22,11 +22,11 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     '''Сущность покемона'''
-    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.PROTECT, null=False, blank=False, related_name='pokemon')
-    lat = models.FloatField(verbose_name='Долгота', null=False, blank=False)
-    lon = models.FloatField(verbose_name='Широта', null=False, blank=False)
-    appeared_at = models.DateTimeField(verbose_name='Появился на карте', null=False, blank=False)
-    disappeared_at = models.DateTimeField(verbose_name='Исчез с карты', null=False, blank=False)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.PROTECT, null=False, blank=False, related_name='entitiesFalse')
+    lat = models.FloatField(verbose_name='Долгота', null=True, blank=True)
+    lon = models.FloatField(verbose_name='Широта', null=True, blank=True)
+    appeared_at = models.DateTimeField(verbose_name='Появился на карте', null=True, blank=True)
+    disappeared_at = models.DateTimeField(verbose_name='Исчез с карты', null=True, blank=True)
     level = models.PositiveIntegerField(verbose_name='Уровень', default=0, blank=True, validators=[MinValueValidator(0)])
     health = models.PositiveIntegerField(verbose_name='Здоровье', default=0, blank=True, validators=[MinValueValidator(0)])
     strength = models.SmallIntegerField(verbose_name='Сила', default=0, blank=True, validators=[MinValueValidator(0)])
